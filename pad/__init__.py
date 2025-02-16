a64cd6704d61ef77d68c92eb8cc2663fc5231b3b
geomaticien <31626726+GeoDF@users.noreply.github.com>
2025-02-16 03:04:49 +0100

    Added a test of the Python version

--- pad/__init__.py
+++ pad/__init__.py
@@ -1,1 +1,7 @@
+import sys
+
+if sys.version_info < (3, 11):
+	print('Freepad require Python version 3.11 or greater, while Python version here is ' + str(sys.version_info.major) + '.' + str(sys.version_info.minor) + '.')
+	sys.exit()
+
 from .freepadapp import FreepadApp
